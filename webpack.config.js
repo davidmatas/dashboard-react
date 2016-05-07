5289e0d2ac81be70de6b671e2e939c5635e851be
David Matas <adavidmatas@gmail.com>
2016-05-07 19:14:52 +0200

    Fix linter warnings in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,23 @@
-var path = require("path");
+var path = require('path');
+
 module.exports = {
   entry: {
-    app: [
-      // "webpack-dev-server/client?http://localhost:8080/",
-      "./app/main.js"
-    ]
+    app: [ './app/main.js' ]
   },
   output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
   },
   devServer: {
     contentBase: 'public/',
     inline: true,
-    colors: true,
+    colors: true
   },
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-      { test: /\.css$/, loader: "style-loader!css-loader" },
-      { test: /\.scss$/, loaders: ["style", "css", "sass"]
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.scss$/, loaders: ['style', 'css', 'sass']
       }
     ]
   }
