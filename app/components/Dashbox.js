098a6e8ad517c767c39139bd3b4cad9bab9b1f16
David Matas <adavidmatas@gmail.com>
2016-05-07 19:09:41 +0200

    Fix linter warnings

--- app/components/Dashbox.js
+++ app/components/Dashbox.js
@@ -1,6 +1,5 @@
 import Multiple from 'multiple.js'
 import React from 'react'
-import ReactDOM from 'react-dom'
 
 const mainColor = 'blue'
 const defaultSize = '33'
--- app/data.js
+++ app/data.js
@@ -1,16 +1,16 @@
 const widgets = [
-  {"size": 40, color: "blue", "value":"16", "unit":"24", "name":"Tasks completed", helper:'true' },
-  {"size": 30, color: "green", "value":"99", "unit":"%", "name":"hapiness" },
-  {"size": 30, color: "green", "value":"🌤", "helper":"true", "unit":"🌔", "name":"wheather" },
-  {"size": 25, color: "blue", "value":"36", "name":"Temperature" },
-  {"size": 25, color: "blue", "value":"98", "unit":"bmp", "name":"Heart Rate" },
-  {"size": 25, color: "green", "value":"12", "unit":"cmHg", "name":"Blood Pressure" },
-  {"size": 25, color: "green", "value":"118", "unit":"mgdl", "name":"Blood Glucose" },
-  {"size": 25, color: "blue", "value":"76", "unit":"kg", "name":"Body Weight" },
-  {"size": 25, color: "blue", "value":"28", "unit":"%", "name":"Fat percentage" },
-  {"size": 25, color: "green", "value":"680", "unit":"kcal", "name":"Avg. consumption" },
-  {"size": 15, color: "green", "value":"85", "unit":"%", "name":"Body Hydration" },
-  {"size": 10, color: "blue", "value":"→"},
+  {'size': 40, color: 'blue', 'value':'16', 'unit':'24', 'name':'Tasks completed', helper:'true' },
+  {'size': 30, color: 'green', 'value':'99', 'unit':'%', 'name':'hapiness' },
+  {'size': 30, color: 'green', 'value':'🌤', 'helper':'true', 'unit':'🌔', 'name':'wheather' },
+  {'size': 25, color: 'blue', 'value':'36', 'name':'Temperature' },
+  {'size': 25, color: 'blue', 'value':'98', 'unit':'bmp', 'name':'Heart Rate' },
+  {'size': 25, color: 'green', 'value':'12', 'unit':'cmHg', 'name':'Blood Pressure' },
+  {'size': 25, color: 'green', 'value':'118', 'unit':'mgdl', 'name':'Blood Glucose' },
+  {'size': 25, color: 'blue', 'value':'76', 'unit':'kg', 'name':'Body Weight' },
+  {'size': 25, color: 'blue', 'value':'28', 'unit':'%', 'name':'Fat percentage' },
+  {'size': 25, color: 'green', 'value':'680', 'unit':'kcal', 'name':'Avg. consumption' },
+  {'size': 15, color: 'green', 'value':'85', 'unit':'%', 'name':'Body Hydration' },
+  {'size': 10, color: 'blue', 'value':'→'}
 ];
 
 export default widgets
